d459259e5c692edf41eceafbac880dbd3b8539b1
vovan <shvv733kn@gmail.com>
2017-04-23 13:12:13 +0300

    routemesg

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@
 Routes and views for the bottle application.
 """
 
-from bottle import route, view, post, request, response
+from bottle import route, view, post, request, response, Bottle
 from datetime import datetime
 import json
 
