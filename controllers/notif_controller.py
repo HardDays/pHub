2a209d1cfd897af9e2991ed7cf9d091b9846abda
vovan <shvv733kn@gmail.com>
2017-04-23 11:50:47 +0300

    not

--- controllers/notif_controller.py
+++ controllers/notif_controller.py
@@ -1,22 +1,26 @@
-from twilio.rest import Client
+import requests
 
 class NotifController(object):
 	def __init__(self):
 		#self.db_collector = db_collector
-		self.client = Client('SKaeabc58042634cd32b65c545ff4cb42b', 'Q6Y2g5mEiZMAYAl3kBSPPA6BvhaSfn2F', 'ACd8d6b302b4c2443c0d5c4900c83be654')
+		self.url = 'https://api.twilio.com/2010-04-01/Accounts/ACd8d6b302b4c2443c0d5c4900c83be654/Messages.json'
+		self.auth = ('ACd8d6b302b4c2443c0d5c4900c83be654', '127b3a7934c97c80cef18e3a4002ce1d')
+		#self.client = Client('SKaeabc58042634cd32b65c545ff4cb42b', 'Q6Y2g5mEiZMAYAl3kBSPPA6BvhaSfn2F', 'ACd8d6b302b4c2443c0d5c4900c83be654')
 
 	def send_to_user(self, user, notification):
 		if user.phone:
-			message = self.client.api.account.messages.create(
-											to=user.phone,
-                                            from_='+12029993558',
-                                            body=notification.form())
+			r = requests.post(self.url, 
+						data={'From': '+12029993558', 'To': user.phone, 'Body': notification.message}, auth=self.auth)
 
 	def send_to_phone(self, phone, message):
+		r = requests.post(self.url, 
+						data={'From': '+12029993558', 'To': phone, 'Body': message}, auth=self.auth)
+		#print(r.status_code, r.reason)
+		'''
 		message = self.client.api.account.messages.create(
 											to=phone,
                                             from_='+12029993558',
-                                            body=message)
+                                            body=message)'''
 
 	def send_to_group(self, group, notification):
 		for user in group.users:
