9a88e8a86b0289d1b1ee30fa46e03f40af8e7764
vovan <shvv733kn@gmail.com>
2017-04-23 14:41:07 +0300

    routemesg

--- controllers/notif_controller.py
+++ controllers/notif_controller.py
@@ -3,20 +3,24 @@ import urllib2, urllib
 class NotifController(object):
     def __init__(self):
         #self.db_collector = db_collector
-        self.url = 'https://api.twilio.com/2010-04-01/Accounts/ACd8d6b302b4c2443c0d5c4900c83be654/Messages.json'
-        self.auth = ('ACd8d6b302b4c2443c0d5c4900c83be654', '127b3a7934c97c80cef18e3a4002ce1d')
+        #self.url = 'https://api.twilio.com/2010-04-01/Accounts/ACd8d6b302b4c2443c0d5c4900c83be654/Messages.json'
+        #self.auth = ('ACd8d6b302b4c2443c0d5c4900c83be654', '127b3a7934c97c80cef18e3a4002ce1d')
+
+        self.url = 'https://api.twilio.com/2010-04-01/Accounts/AC30d84becc930c90cb2c2bf01ea6a99ab/Messages.json'
+        self.auth = ('AC30d84becc930c90cb2c2bf01ea6a99ab', 'ac118f8179fcd39274ddd982892853d0')
         #self.client = Client('SKaeabc58042634cd32b65c545ff4cb42b', 'Q6Y2g5mEiZMAYAl3kBSPPA6BvhaSfn2F', 'ACd8d6b302b4c2443c0d5c4900c83be654')
 
     def send_to_user(self, user, notification):
+        #+12029993558
         if user.phone:
             r = requests.post(self.url, 
-                        data={'From': '+12029993558', 'To': user.phone, 'Body': notification.message}, auth=self.auth)
+                        data={'From': '+16123602818', 'To': user.phone, 'Body': notification.message}, auth=self.auth)
 
     def send_to_phone(self, phone, message):
         #r = requests.post(self.url, 
                        # data={'From': '+12029993558', 'To': phone, 'Body': message}, auth=self.auth)
 
-        data = urllib.urlencode({'From': '+12029993558', 'To': phone, 'Body': message})
+        data = urllib.urlencode({'From': '+16123602818', 'To': phone, 'Body': message})
         passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
         passman.add_password(None, self.url, self.auth[0], self.auth[1])
         auth_handler = urllib2.HTTPBasicAuthHandler(passman)
